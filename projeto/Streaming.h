9b6b60b8afbb665b5ab2a3cbf5174e9da09a3344
Dcorder123 <vasconcelos.barbosa@academico.ifpb.edu.br>
2022-12-13 14:50:12 -0300

    deletando arquivos antigos e  modifiquei straming

--- projeto/Streaming.h
+++ projeto/Streaming.h
@@ -1,22 +1,25 @@
 #ifndef STREAMING_H
 #define STREAMING_H
+
 #include "App.h"
 #include "Producao.h"
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 using std::vector;
+
 class Streaming : public App
 {
     friend ostream &operator<<(ostream &out, Streaming &s);
 
 public:
-    Streaming();
-    Producao &operetor(const Producao &p);
-    Producao &operetor (const Producao &p);
-
+    Streaming(string nome, string versao);
+    Producao &operator << (Producao &p);//adiciona producao
+    Producao &operator >> (Producao &p);//remove producao
+    virtual void run();//executa o app exibir as producoes e o usuario escolhe uma para assistir
 protected:
-    vector<Producao&> &producoes;
+    vector<Producao*> producoes;
     string nome;
 };
 
