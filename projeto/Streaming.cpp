02601493a69c7b2cfc18f70b3e505f0c54fdaae7
Dcorder123 <vasconcelos.barbosa@academico.ifpb.edu.br>
2022-12-13 14:47:44 -0300

    excluir datados e modifiquei straming

---
+++ projeto/Streaming.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "Streaming.h"
+#include "Producao.h"
+#include "Filme.h"
+#include "Series.h"
+
+using namespace std;
+
+Streaming::Streaming(string nome, string versao) : App()
+{
+    this->nome = nome;
+    this->versao = versao;
+}
+
+Producao &Streaming::operator << (Producao &p)
+{
+    producoes.push_back(&p);
+    return p;
+}
+
+Producao &Streaming::operator >> (Producao &p)
+{
+    for(int i = 0; i < producoes.size(); i++)
+    {
+        if(producoes[i] == &p)
+        {
+            producoes.erase(producoes.begin() + i);
+        }
+    }
+    return p;
+}
+
+void Streaming::run()
+{
+    cout << "Streaming: " << nome << endl;
+    cout << "Versao: " << versao << endl;
+    cout << "Producoes: " << endl;
+    for(int i = 0; i < producoes.size(); i++)
+    {
+        cout << i << " - " << producoes[i]->getNome() << endl;
+    }
+    cout << "Escolha uma producao para assistir: " << endl;
+    int escolha;
+    cin >> escolha;
+    producoes[escolha]->play();
+}
+
+
+
+// Path: projeto/Streaming.h
